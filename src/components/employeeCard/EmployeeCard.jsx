56e8acda58abff586d739b664e1d4b12ba27062d
pluxxury <pedobirandfriends@gmail.com>
2024-06-30 17:59:14 +0300

    getSection layout

---
+++ src/components/employeeCard/EmployeeCard.jsx
@@ -0,0 +1,11 @@
+import "./EmployeeCard.scss";
+
+const EmployeeCard = ({ employee }) => {
+    return (
+        <div className="employeeCard">
+            qweqweq
+        </div>
+    )
+};
+
+export default EmployeeCard;
--- src/components/getSection/GetSection.jsx
+++ src/components/getSection/GetSection.jsx
@@ -1,11 +1,22 @@
-import "./GetSection.scss"
+import EmployeeCard from "../employeeCard/EmployeeCard";
+
+import "./GetSection.scss";
 
 const GetSection = () => {
     return (
-        <section className="GetSection">
+        <section className="getSection">
             <h1>Working with GET request</h1>
+            <div className="cards__wrapper">
+                <EmployeeCard />
+                <EmployeeCard />
+                <EmployeeCard />
+                <EmployeeCard />
+                <EmployeeCard />
+                <EmployeeCard />
+            </div>
+            <button className="button">Show more</button>
         </section>
-    )
+    );
 };
 
 export default GetSection;
